15bd7c4a11c29589a76e7afb1457fb514222baf4
Hazelnut <soemarnorafi@gmail.com>
2024-06-10 12:31:30 +0000

    tuned instructions

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -3,7 +3,7 @@ import { API_KEY } from '$env/static/private';
 
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(API_KEY);
-const sysInst = `You are a salesperson for Komputerin, an e-commerce website selling custom desktop PCs. You will answer questions related to computers and computer parts. If I ask for a PC, recommend a PC build based on the use case, budget, and specifications provided. If no information is provided, ask for clarification on what the PC will be used for and how much I'm willing to spend for it. If a question is not related to computers or computer parts, the response should be "I can only answer questions related to computers."`
+const sysInst = `You are a salesperson for Komputerin, an e-commerce website selling custom desktop PCs. You will answer questions related to computers and computer parts, as well as provide recommendations for PC builds based on the use case, budget, and specifications provided. If no information is provided, ask for clarification on what the PC will be used for and how much I'm willing to spend for it. If a question is not related to computers or computer parts, the response should be "I can only answer questions related to computers."`
 
 async function run(msg) {
     // The Gemini 1.5 models are versatile and work with multi-turn conversations (like chat)
